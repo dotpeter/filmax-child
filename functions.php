1db647edaeb22c069260e250c38a6d0415280b2b
Pedro <pedro@dig.id>
2025-12-10 11:25:21 +0000

    up

--- functions.php
+++ functions.php
@@ -1,20 +1,24 @@
 <?php
-
-add_action( 'wp_enqueue_scripts', 'irrev_theme_enqueue_styles' );
+/**
+ * Child-Theme functions and definitions
+ *
+ * @package filmax-child
+ */
 
 /**
  * Enqueue styles and scripts.
  *
  * @return void
  */
-function irrev_theme_enqueue_styles() {
-
+function filmax_child_scripts() {
 	$the_theme     = wp_get_theme();
 	$theme_version = $the_theme->get( 'Version' );
 
+	wp_enqueue_style( 'filmax-parent-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'filmax-child-style', get_stylesheet_uri(), array(), $theme_version );
 	wp_enqueue_style( 'irreversivel-style', get_stylesheet_uri() . '/dist/css/main.css', array(), $theme_version );
-
 }
 
-require_once get_theme_file_path( 'inc/theme-admin.php' );
+add_action( 'wp_enqueue_scripts', 'filmax_child_scripts' );
+
+//require_once get_theme_file_path( 'inc/theme-admin.php' );
